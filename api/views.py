3d48da18f50662b8059b93f032805db90ebc18a2
gtsgit <belyaevma@surgut.gazprom.ru>
2024-02-08 00:16:11 +0500

    aa

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .serializers import *
 from .models import *
@@ -38,4 +38,4 @@ class AchievementViewSet(viewsets.ModelViewSet):
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
